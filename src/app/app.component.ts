f0930b2ebd2f36b197e2439cb4b6f9896d18df26
Gustavo Gonzales <gustavo.gonda@gmail.com>
2020-01-21 11:42:15 -0500

    primer comit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'myapp';
+  searchText;
+  users = [
+  {
+    name:"gustavo",
+    lastname:"gonzales",
+    email:"gustavo@gmail.com",
+    usergroup:"registered",
+    lastvisit:"31/07/2006"
+  },
+  {
+    name:"omar",
+    lastname:"gonzales",
+    email:"omar@gmail.com",
+    usergroup:"registered",
+    lastvisit:"31/07/2006"
+
+  },
+  {
+    name:"erick",
+    lastname:"gonzales",
+    email:"erick@gmail.com",
+    usergroup:"registered",
+    lastvisit:"30/07/2006"
+
+  }
+]
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    Ng2SearchPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
